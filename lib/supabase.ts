5ba27b6fbde1c4cc7d63ff1cb45a45d445d50a5b
Ruben Donis <rubendonis@Rubens-MacBook-Pro.local>
2026-05-18 10:24:24 -0600

    fixed supabase deployment

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
 
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Supabase environment variables missing.");
+}
+
+export const supabase =
+  supabaseUrl && supabaseAnonKey
+    ? createClient(
+        supabaseUrl,
+        supabaseAnonKey
+      )
+    : null;
